7391fa9ec0579b68e8b600273d9ae3799f76d439
joshblevins1985 <Medic!283>
2021-04-26 17:09:03 -0400

    initial

--- resources/views/logistics/drugBagIndex.blade.php
+++ resources/views/logistics/drugBagIndex.blade.php
@@ -82,8 +82,8 @@
                     <tbody>
                     @foreach($drugBags as $d)
                         <tr>
-                            <td>{{$d->station->description}}</td>
-                            <td>{{$d->description ?? ''}}</td>
+                            <td>{{$d->station->station}}</td>
+                            <td>{{$d->bag_number ?? ''}}</td>
                             <td>{{$d->currentSealNumber ?? 'Unknown'}}</td>
                             <td>@if($d->bagExpires) {{\Carbon\Carbon::parse($d->bagExpires)->format('m-d-Y')}} @else Unknown @endif</td>
                             <td>{{$d->lastAssigned->employee->first_name ?? 'Never Assigned'}} {{$d->lastAssigned->employee->last_name ?? ''}}</td>
