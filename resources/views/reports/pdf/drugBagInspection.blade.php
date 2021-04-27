cbb48a6211fe95c3172c8982183b1cc162bd9d60
joshblevins1985 <Medic!283>
2021-04-27 14:37:17 -0400

    initial

--- resources/views/reports/pdf/drugBagInspection.blade.php
+++ resources/views/reports/pdf/drugBagInspection.blade.php
@@ -8,7 +8,19 @@
 
     <div class="row">
         <div class="col-7 text-center">
-            <img src="{{ public_path('assets/light/img/logo.png') }}" alt="Medic Dispatch" height="50"> <span style="font-weight: bold; font-style: italic; font-size: 20pt;" > Medidex|Solutions</span>
+            <div class="row">
+                <img src="{{ public_path('assets/light/img/logo.png') }}" alt="Medic Dispatch" height="50"><span style="font-weight: bold; font-style: italic; font-size: 20pt;" > Medidex|Solutions</span>
+            </div>
+             <div class="row">
+                 <table class="table">
+                     <tr>
+                         <td>Old Seal</td>
+                         <td>{{$inspection->items['baginfo']['oldSeal']}}</td>
+                         <td>New Seal</td>
+                         <td>{{$inspection->items['baginfo']['newSeal']}}</td>
+                     </tr>
+                 </table>
+             </div>
         </div>
         <div class="col-5">
             <table class="table">
