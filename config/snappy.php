d666741034e5ded3b3eafe39d3b9ed4a7bd7288c
joshblevins1985 <Medic!283>
2022-03-28 10:46:02 -0400

    initial

--- config/snappy.php
+++ config/snappy.php
@@ -7,12 +7,14 @@ return array(
         'enabled' => true,
         'binary' => base_path('vendor/h4cc/wkhtmltopdf-amd64/bin/wkhtmltopdf-amd64'),
         'timeout' => false,
-        'options' => array(),
+        'options' => array(
+            'enable-local-file-access' => true,
+        ),
         'env'     => array(),
     ),
     'image' => array(
         'enabled' => true,
-        'binary' => base_path('vendor/h4cc/wkhtmltoimage-amd64/bin/wkhtmltoimage-amd64'),
+        'binary'  => 'vendor/h4cc/wkhtmltoimage-amd64/wkhtmltoimage',
         'timeout' => false,
         'options' => array(),
         'env'     => array(),
